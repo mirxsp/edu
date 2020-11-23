28bcb6bbf1c205af78109f10577a6fa55f30257b
mirxsp <65396203+mirxsp@users.noreply.github.com>
2020-11-23 21:07:57 +0300

    Add files via upload

---
+++ Chocolate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3
+{
+    class Chocolate : Sweet
+    {
+        public enum Types
+        {
+            Dark,
+            Milk,
+            White,
+            Mixed
+        };
+        public Types Type { get; set; }
+        public double PalmOilAmount { get; set; }
+
+        public double getPalmOilPercentage()
+        {
+            return PalmOilAmount / Weight * 100;
+        }
+    }
+}
